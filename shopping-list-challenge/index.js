06b1a6c14c67db24e7605e235e2b8afbd9b2b596
Sean Phelan <seanphelan97@gmail.com>
2018-08-23 13:06:24 -0600

    added some event handling

--- shopping-list-challenge/index.js
+++ shopping-list-challenge/index.js
@@ -1,7 +1,7 @@
 
 function generateItemHtml(itemName) {
     return `<li>
-        < span class = "shopping-item" > ${itemName}< /span>
+        <span class="shopping-item">${itemName}</span>
         <div class="shopping-item-controls">
             <button class="shopping-item-toggle">
                 <span class="button-label">check</span>
@@ -22,6 +22,15 @@ function handleShoppingCart() {
         let itemName = $('.js-shopping-list-entry').val();
         shoppingList.append(generateItemHtml(itemName));
     });
+
+     $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        console.log('event ran');
+        let item = $().parent().find('.shopping-item');
+        console.log(item);
+        item.addClass('shopping-item__checked');
+     });
+
+    // console.log($("shopping-item-toggle").parent().find('.shopping-item'));
 }
 
 $(handleShoppingCart);
